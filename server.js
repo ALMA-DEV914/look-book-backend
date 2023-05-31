6857344f090c0deedd3a514fd708ac97e7c61604
Alma Braun <aungonalna58@gmail.com>
2023-05-30 18:09:25 -0700

    Update server.js

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ const path = require("path");
 
 //defining mongoose options
 const corsOptions = {
-  origin: ["http://localhost:3000" , "https://look-book-act-group42.herokuapp.com/"],
+  origin: ["http://localhost:3000" , "https://look-book.onrender.com/"],
   preflightContinue:false,
   credentials: true,
   optionSuccessStatus: 200,
